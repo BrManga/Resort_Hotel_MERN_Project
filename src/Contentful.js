f86f05d173b43e761ca7fdc79100a11deb11aabb
Bora Manga <bora.mng525@gmail.com>
2019-11-06 14:07:01 +0100

    connection made with contentful

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import { createClient } from "contentful";
+
+export default createClient({
+  space: "5we415dg433k",
+  accessToken: "N9TolTTqxYPpoXd501XNDM276YfUmxxVnqXu4clqOZY"
+});
--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import items from "./data";
-import { isMixedTypeAnnotation } from "@babel/types";
+//import items from "./data";
+import Client from "./Contentful";
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
@@ -19,21 +19,33 @@ class RoomProvider extends Component {
     breakfast: false,
     pets: false
   };
-  componentDidMount() {
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true);
-    let maxPrice = Math.max(...rooms.map(item => item.price));
-    let maxSize = Math.max(...rooms.map(item => item.size));
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: "beachResortRoom",
+        order: "fields.price"
+      });
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
 
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      price: maxPrice,
-      maxPrice: maxPrice,
-      maxSize: maxSize
-    });
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        price: maxPrice,
+        maxPrice: maxPrice,
+        maxSize: maxSize
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getData();
   }
   formatData(items) {
     let tempItems = items.map(item => {
